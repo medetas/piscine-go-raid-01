e8c52a23f6a146211892edf3b11951f9f0407956
medetas <masylzhan07@gmail.com>
2019-10-19 17:18:57 +0600

    raid1b

--- raid1b.go
+++ raid1b.go
@@ -3,7 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func Raid1b(x, y int) {
-	if x > 0 || y > 0 {
+	if x < 0 || y < 0 {
+		return
+	} else {
 		for i := 0; i < y; i++ {
 			for j := 0; j < x; j++ {
 				if (i == 0 && j == 0) || (i == y-1 && j == x-1) {
