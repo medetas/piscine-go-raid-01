9515565a9372c0bfc42d9e95cac5b30980eeb913
medetas <masylzhan07@gmail.com>
2019-10-19 18:15:57 +0600

    raid1d

---
+++ raid1d.go
@@ -0,0 +1,25 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func Raid1d(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	} else {
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				if j == 0 && (i == y-1 || i == 0) {
+					z01.PrintRune(65)
+				} else if j == x-1 && (i == y-1 || i == 0) {
+					z01.PrintRune(67)
+				} else if i != 0 && j != 0 && i != y-1 && j != x-1 {
+					z01.PrintRune(32)
+				} else {
+					z01.PrintRune(66)
+				}
+
+			}
+			z01.PrintRune(10)
+		}
+	}
+}
