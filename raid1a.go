d15e6726b35c60868d592090e1422ab6997007d1
medetas <masylzhan07@gmail.com>
2019-10-19 17:00:04 +0600

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -1,20 +1,21 @@
 package piscine
+
 import "github.com/01-edu/z01"
 
-func Raid1a(x,y int) {
+func Raid1a(x, y int) {
 	if x > 0 || y > 0 {
 		for i := 0; i < y; i++ {
 			for j := 0; j < x; j++ {
-				if (i == 0 && (j == x - 1  || j == 0)) || (i == y-1 && (j == x - 1  || j == 0))  {
+				if (i == 0 && (j == x-1 || j == 0)) || (i == y-1 && (j == x-1 || j == 0)) {
 					z01.PrintRune(111)
-				}else if (j == 0 && (i != 0 || i != y - 1)) || (j == x - 1 && (i != 0 || i != y - 1)) {
+				} else if (j == 0 && (i != 0 || i != y-1)) || (j == x-1 && (i != 0 || i != y-1)) {
 					z01.PrintRune(124)
-				}else if (i != 0 && j != 0 && i != y - 1 && j != x - 1) {
+				} else if i != 0 && j != 0 && i != y-1 && j != x-1 {
 					z01.PrintRune(32)
-				}else {
+				} else {
 					z01.PrintRune(45)
 				}
-				
+
 			}
 			z01.PrintRune(10)
 		}
