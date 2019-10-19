2ba22cb6d41a1e058b932dd9d59df4d1c0c7d7a2
callme-max <43644249+callme-max@users.noreply.github.com>
2019-10-19 18:22:45 +0600

    Update raid1e.go

--- raid1e.go
+++ raid1e.go
@@ -3,10 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func Raid1e(x, y int) {
-	if x > 0 || y > 0 {
+	if x < 0 || y < 0 {
+		return
+	} else {
 		for i := 0; i < y; i++ {
 			for j := 0; j < x; j++ {
-				if (i == 0 && j == 0) || (i == y-1 && j == x-1) {
+				if (i == 0 && j == 0) || (i == y-1 && j == x-1 && i != 0 && j != 0) {
 					z01.PrintRune(65)
 				} else if (j == x-1 && i == 0) || (i == y-1 && j == 0) {
 					z01.PrintRune(67)
